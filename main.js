d03f3b4690fc72e48106466444da87335dae9465
Shouta <bob1996w@gmail.com>
2018-07-21 23:44:43 +0800

    fix-test

---
+++ main.js
@@ -0,0 +1,12 @@
+(function () {
+  var globalVars = {
+    statusMode = 0,
+    musicBlobUrl = "",
+    dataBlobUrl = "",
+    dataObj = {}
+  };
+  
+  window.gv = globalVars;
+
+  console.log('main.js invoked');
+}) ();
